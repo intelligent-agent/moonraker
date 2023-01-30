f0141f564abd427fb5b64cb8a127fba037cf082d
Elias Bakken <elias@iagent.no>
2023-01-31 00:00:10 +0100

    Clean up code

--- moonraker/components/recore.py
+++ moonraker/components/recore.py
@@ -6,52 +6,32 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
 from __future__ import annotations
-import sys
 import logging
 import asyncio
-import time
-import getpass
 
 # Annotation imports
 from typing import (
     TYPE_CHECKING,
     Any,
-    Awaitable,
-    Callable,
     Dict,
-    List,
-    Optional,
-    Tuple,
-    Union
+    Optional
 )
 
 if TYPE_CHECKING:
     from confighelper import ConfigHelper
     from websockets import WebRequest
     from .shell_command import ShellCommandFactory as SCMDComp
-    from .authorization import Authorization
-    from .announcements import Announcements
-    SudoReturn = Union[Awaitable[Tuple[str, bool]], Tuple[str, bool]]
-    SudoCallback = Callable[[], SudoReturn]
 
 class Recore:
     def __init__(self, config: ConfigHelper) -> None:
         self.server = config.get_server()
 
-        pclass = BaseProvider
-        self.sys_provider: BaseProvider = pclass(config)
-
         self.server.register_endpoint(
-            "/recore/enable_ssh", ['POST'], self._handle_machine_request)
+            "/recore/enable_ssh", ['POST'], self._handle_enable_ssh_request)
         self.server.register_endpoint(
-            "/recore/set_boot_media", ['POST'], self._handle_machine_request)
+            "/recore/set_boot_media", ['POST'], self._handle_boot_media_request)
         self.server.register_endpoint(
             "/recore/state", ['GET'], self._handle_recore_state_request)
-        # Register remote methods
-        self.server.register_remote_method(
-            "enable_ssh", self.sys_provider.enable_ssh)
-        self.server.register_remote_method(
-            "set_boot_media", self.sys_provider.set_boot_media)
 
         shell_cmd: SCMDComp = self.server.load_component(
             config, 'shell_command')
@@ -74,21 +54,23 @@ class Recore:
     async def component_init(self) -> None:
         self.init_evt.set()
 
-    async def _handle_machine_request(self, web_request: WebRequest) -> str:
-        ep = web_request.get_endpoint()
-        if ep == "/recore/enable_ssh":
-            value = web_request.get("value")
-            await self.sys_provider.enable_ssh(value)
-        elif ep == "/recore/set_boot_media":
-            value = web_request.get("value")
-            await self.sys_provider.set_boot_media(value)
-        else:
-            raise self.server.error("Unsupported machine request")
+    async def _exec_sudo_command(self, command: str):
+        machine: Machine = self.server.lookup_component("machine")
+        return await machine.exec_sudo_command(command, timeout=5)
+
+    async def _handle_enable_ssh_request(self, web_request: WebRequest) -> str:
+        enabled = web_request.get("value")
+        await self._exec_sudo_command(
+            f"/usr/local/bin/set-ssh-access {enabled}")
         return "ok"
 
-    async def _handle_recore_state_request(self,
-                                      web_request: WebRequest
-                                      ) -> Dict[str, Any]:
+    async def _handle_boot_media_request(self, web_request: WebRequest) -> str:
+        media = web_request.get("value")
+        await self._exec_sudo_command(f"/usr/local/bin/set-boot-media {media}")
+        return "ok"
+
+    async def _handle_recore_state_request(self, web_request: WebRequest
+                                           ) -> Dict[str, Any]:
         boot_media = await self._get_boot_media()
         ssh_enabled = await self._get_ssh_enabled()
         recore_state = {
@@ -100,7 +82,8 @@ class Recore:
     async def _get_boot_media(self) -> str:
         shell_cmd: SCMDComp = self.server.lookup_component('shell_command')
         try:
-            resp = await self.get_boot_media_cmd.run_with_response(log_complete=False)
+            resp = await self.get_boot_media_cmd.run_with_response(
+                log_complete=False)
         except shell_cmd.error:
             return "Error: setting boot media failed"
         if resp:
@@ -110,27 +93,13 @@ class Recore:
     async def _get_ssh_enabled(self) -> str:
         shell_cmd: SCMDComp = self.server.lookup_component('shell_command')
         try:
-            resp = await self.is_ssh_enabled_cmd.run_with_response(log_complete=False)
+            resp = await self.is_ssh_enabled_cmd.run_with_response(
+                log_complete=False)
         except shell_cmd.error:
             return "Error: setting SSH access failed"
         if resp:
             return resp.strip()
         return "unknown"
-
-
-class BaseProvider:
-    def __init__(self, config: ConfigHelper) -> None:
-        self.server = config.get_server()
-
-    async def _exec_sudo_command(self, command: str):
-        machine: Machine = self.server.lookup_component("machine")
-        return await machine.exec_sudo_command(command, timeout=5)
-
-    async def enable_ssh(self, enabled) -> None:
-        await self._exec_sudo_command(f"/usr/local/bin/set-ssh-access {enabled}")
-
-    async def set_boot_media(self, media) -> None:
-        await self._exec_sudo_command(f"/usr/local/bin/set-boot-media {media}")
 
 def load_component(config: ConfigHelper) -> Recore:
     return Recore(config)
